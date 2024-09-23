1aef224e63ebe2cdf9611bdb496617bb7302f013
Muntazir-sd <sayedmuntazir007@gmail.com>
2024-09-23 10:18:25 +0530

    made a handleasync global function and added apiResponse and Apierror class to standadize everything

---
+++ src/utils/HandleAsync.utils.js
@@ -0,0 +1,10 @@
+// Error handling wrapper function
+export const handleAsync = (fn) => {
+    return async (req, res, next) => {
+        try {
+            await fn(req, res, next);
+        } catch (error) {
+            next(error);
+        }
+    };
+};
